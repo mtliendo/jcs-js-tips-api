ba2fd2d0690d058a156592dafd33ca40dfd51d99
Oghenebrume50 <raphlbrume@gmail.com>
2019-07-11 16:39:58 +0100

    changed the routes to use a same uri the content are served up as long as the uri is correct even without the tags value client will no longer get **no such content** but all the available contents

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const { fetchJCsJSTipsTweets } = require('./fetchJCsJSTipsTweets')
 const app = express()
 const PORT = process.env.PORT || 8888
 
-app.get('/jcs-js-tips', async (req, res) => {
-  const tipsJSON = await fetchJCsJSTipsTweets()
-  res.json(tipsJSON)
-})
-
-app.get('/js-lessons', async (req, res) => {
+app.get('/jcsjs-lessons', async (req, res) => {
+  let tipsJSON = await fetchJCsJSTipsTweets()
   if (req.query.tags) {
-    let tags = req.query.tags.split(',')
-    let tipsJSON = await fetchJCsJSTipsTweets()
+    const tags = req.query.tags.split(',')
     tipsJSON = tipsJSON.filter((tip) => {
       if (tags.some((tag) => tip.tweetText.toLowerCase().includes(tag.toLowerCase()))) {
         return tip
@@ -22,7 +17,7 @@ app.get('/js-lessons', async (req, res) => {
     return res.json(tipsJSON) 
   }
   
-  res.json("no such content")
+  res.json(tipsJSON)
 })
 
 app.listen(PORT, () => {
